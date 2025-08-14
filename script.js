7dec6f15ffc3768562123bf89dd338077caee9a5
eduardo-sprachum <145134393+eduardo-sprachum@users.noreply.github.com>
2025-08-14 11:45:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-fhhhfhmhjhjfh
+const caixaPrincipal = document.querySelector(".caixaPrincipal");
+const caixaPerguntas = document.querySelector("caixaPerguntas");
+const caixaAltrnativas = document.querySelector("caixaAlternativas");  
+const caixaResultado = document.querySelector("caixaResultado");
+const textoResultado = document.querySelector("textoResultado");
