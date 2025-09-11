e5f0940cfe58e775c883fa23cb23c0aa6ff3e190
eduardo-sprachum <145134393+eduardo-sprachum@users.noreply.github.com>
2025-09-11 11:50:47 -0300

    .

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const caixaPrincipal = document.querySelector(".caixaPrincipal");
 const caixaPerguntas = document.querySelector(".caixaPerguntas");
-const caixaAltrnativas = document.querySelector(".caixaAlternativas");  
+const caixaAlternativas = document.querySelector(".caixaAlternativas");  
 const caixaResultado = document.querySelector(".caixaResultado");
 const textoResultado = document.querySelector(".textoResultado");
 
